97e0e0de2da2b065954425b953af119a4014ddca
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2017-03-29 08:50:09 -0400

    Base classes

--- ldpl.py
+++ ldpl.py
@@ -16,13 +16,16 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 class ldpl_based_trilateration(object):
     """docstring for ldpl_based_trilateration."""
-    def __init__(self, measurement, grid_labels, pos_node, pt=24, **kwargs):
+    def __init__(self, measurement, grid_labels, pos_node, pt=24, pr_d0=0):
         super(ldpl_based_trilateration, self).__init__()
         self.measurement = measurement
         self.grid_labels = grid_labels
         self.pt = pt
+        self.pl_d0 = self.pt - pr_d0;
         self.pos_node = pos_node
         self.mean_measurements = None
+        self.pl = calculate_path_loss(self.self.measurement)
+
 
     def calculate_path_loss(self, pt, pr):
         return pt-pr
@@ -44,6 +47,163 @@ class ldpl_based_trilateration(object):
     def create_gaussian(self):
         x = np.linspace(-4,4,100)
         return norm.pdf(x)
+
+    ''' Properties '''
+    def pl_d0():
+        doc = "The pl_d0 property."
+        def fget(self):
+            return self._pl_d0
+        def fset(self, value):
+            self._pl_d0 = value
+        def fdel(self):
+            del self._pl_d0
+        return locals()
+        pl_d0 = property(**pl_d0())
+    def pl():
+        doc = "The pl property."
+        def fget(self):
+            return self._pl
+        def fset(self, value):
+            self._pl = value
+        def fdel(self):
+            del self._pl
+        return locals()
+        pl = property(**pl())
+
+    def measurement():
+        doc = "The measurement property."
+        def fget(self):
+            return self._measurement
+        def fset(self, value):
+            self._measurement = value
+        def fdel(self):
+            del self._measurement
+        return locals()
+    measurement = property(**measurement())
+
+    def grid_labels():
+        doc = "The grid_labels property."
+        def fget(self):
+            return self._grid_labels
+        def fset(self, value):
+            self._grid_labels = value
+        def fdel(self):
+            del self._grid_labels
+        return locals()
+
+    grid_labels = property(**grid_labels())
+
+    def pt():
+        doc = "The pt property."
+        def fget(self):
+            return self._pt
+        def fset(self, value):
+            self._pt = value
+        def fdel(self):
+            del self._pt
+        return locals()
+    pt = property(**pt())
+
+class grid_cells(object):
+    """docstring for grid_cells."""
+    def __init__(self, nx=25, ny=8, sx=0.9, sy=0.9, n_node = 8):
+        super(grid_cells, self).__init__()
+        self.nx = nx
+        self.ny = ny
+        self.sx = sx
+        self.sy = sy
+        self.n_node = n_node
+        self.cells = np.zeros((self.nx, self.ny, self.n_node))
+        self.centers_x, self.centers_y, self.centers = self.create_grid_centers(self.nx, self.ny, self.sx, self.sy)
+
+    def mean_measurements(self, measurements, labels):
+        n_obs = np.zeros_like(self.cells)
+        cells = np.zeros_like(self.cells)
+        for i in range(measurements.shape[0]):
+            for ap in range(self.n_node):
+                cells[labels[i,0], labels[i,1], ap] += measurements[i, ap]
+                if measurements[i, ap] != 0:
+                    n_obs[labels[i,0], labels[i,1], ap] += 1
+        np.seterr(divide="ignore")
+        self.cells = np.divide(cells, n_obs)
+        # np.nan_to_num(self.cells)
+        return self.cells
+
+    def create_grid_centers(self, nx, ny, sx, sy):
+        x = np.arange(nx)
+        y = np.arange(ny)
+        gcx = sx/2 + x*sx
+        gcy = sy/2 + y*sy
+        xx, yy = np.meshgrid(gcx, gcy)
+        # print np.vstack((xx.ravel(), yy.ravel())).T.shape
+        centers = np.vstack((xx.ravel(), yy.ravel())).T.reshape((nx, ny, 2))
+        return (gcx, gcy, centers)
+
+    def centers():
+        doc = "The centers property."
+        def fget(self):
+            return self._centers
+        def fset(self, value):
+            self._centers = value
+        def fdel(self):
+            del self._centers
+        return locals()
+    centers = property(**centers())
+
+    def cells():
+        doc = "The cells property."
+        def fget(self):
+            return self._cells
+        def fset(self, value):
+            self._cells = value
+        def fdel(self):
+            del self._cells
+        return locals()
+        cells = property(**cells())
+
+    def nx():
+        doc = "The nx property."
+        def fget(self):
+            return self._nx
+        def fset(self, value):
+            self._nx = value
+        def fdel(self):
+            del self._nx
+        return locals()
+    nx = property(**nx())
+
+    def ny():
+        doc = "The ny property."
+        def fget(self):
+            return self._ny
+        def fset(self, value):
+            self._ny = value
+        def fdel(self):
+            del self._ny
+        return locals()
+    ny = property(**ny())
+
+    def gx():
+        doc = "The gx property."
+        def fget(self):
+            return self._gx
+        def fset(self, value):
+            self._gx = value
+        def fdel(self):
+            del self._gx
+        return locals()
+    gx = property(**gx())
+
+    def gy():
+        doc = "The gy property."
+        def fget(self):
+            return self._gy
+        def fset(self, value):
+            self._gy = value
+        def fdel(self):
+            del self._gy
+        return locals()
+    gy = property(**gy())
 
 class residual_analysis(object):
     """docstring for residual_analysis."""
--- ldpl_based_trilateration.py
+++ ldpl_based_trilateration.py
@@ -6,21 +6,50 @@ import numpy.matlib
 import scipy as sp
 import os
 
-from sklearn import mixture
-from sklearn.manifold import TSNE
-
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from ldpl import ldpl_based_trilateration
+from ldpl import grid_cells
 from ldpl import residual_analysis
 from ldpl import visualization_tool
+
+from wifi_data import wifi_data as db
+
+def print_dataset_shapes(dataset):
+    print "WiFi Set: ", dataset.wifi_train.shape, dataset.wifi_test.shape
+    print "BT Set: ", dataset.bt_train.shape, dataset.bt_test.shape
+    print "LoRa Set: ", dataset.lora_train.shape, dataset.lora_test.shape
+
+    print "Grid Labels (by XY): ", dataset.grid_labels_train.shape, dataset.grid_labels_test.shape
+    print "Grid Labels (by #): ", dataset.grid_numbers_train.shape, dataset.grid_numbers_test.shape
+    print "Min and Max X Label: ", np.amin(dataset.__grid_labels_by_xy__[:,0]), np.amax(dataset.__grid_labels_by_xy__[:,0])
+    print "Min and Max Y Label: ", np.amin(dataset.__grid_labels_by_xy__[:,1]), np.amax(dataset.__grid_labels_by_xy__[:,1])
+
 def main():
+    fname_dataset = "hancock_data.mat"
+    fname_model = "grid_classifier.h5"
+    folderLocation = os.path.dirname(os.path.realpath(__file__))
+    # Create the dataset
+    dataset = db(folder_location=folderLocation, filename=fname_dataset, normalize=False, missingValues=0, nTraining=1200, nTesting=343, nValidation=0, verbose=False)
+
+    print_dataset_shapes(dataset)
+    ''' pos_node_vis is just used for visualization purposes, not used in any calculations '''
+    pos_node_vis = np.array([[0, 0], [8, 0], [16, 0], [25, 0], [25, 8], [16, 9], [8, 9], [0, 9]])
+    ref_grids = np.array([[0, 2], [8, 2], [16, 2], [24, 2], [24, 6], [16, 6], [8, 6], [0, 6]])
+    pos_node = np.array([[0, 0], [7.2000, 0], [14.4000, 0], [22.5000, 0], [22.5000, 7.2000], [14.4000, 7.2000], [7.2000, 7.2000], [0, 7.2000]]);
+    ldpl_wifi = ldpl_based_trilateration(measurement=dataset.__wifi_data__, grid_labels=dataset.__grid_labels_by_xy__, pt=15, pos_node=pos_node)
+
+    wifi_grids = grid_cells()
+    ldpl_wifi.mean_measurements = wifi_grids.mean_measurements(dataset.__wifi_data__, dataset.__grid_labels_by_xy__)
+    bt_grids = grid_cells()
+    prop_bt = bt_grids.mean_measurements(dataset.__bt_data__, dataset.__grid_labels_by_xy__)
+    lora_grids = grid_cells()
+    prop_lora = lora_grids.mean_measurements(dataset.__lora_data__, dataset.__grid_labels_by_xy__)
+
     res_analysis = residual_analysis(error="euclidean")
-    print res_analysis.error
-    # ldpl_model = ldpl_based_trilateration()
 
 if __name__ == "__main__":
     main()
